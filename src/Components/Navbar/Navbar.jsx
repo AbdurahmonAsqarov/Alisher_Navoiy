669596f5512c0e1153064607e8319d82fb825de3
abdurahmon <abdurahmongit1@gmail.com>
2024-10-14 22:51:45 +0500

    part 1.4

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,51 +18,53 @@ const Navbar = () => {
 
   return (
     <>
-      <div className='container fixed'>
-        <nav className='flex justify-between px-[5vw] items-center bg-[#2d303f] border-b-2 border-[#af8b67]'>
-          <img src={book_img} alt="Book Story" className='w-[80px]' />
+      <div>
+        <div className='container fixed'>
+          <nav className='flex justify-between px-[5vw] items-center bg-[#2d303f] border-b-2 border-[#af8b67]'>
+            <img src={book_img} alt="Book Story" className='w-[80px]' />
 
-          <motion.div
-            animate={burgerIcon ? { rotate: -90, opacity: 1 } : { rotate: 0, opacity: 0.8 }}
-            transition={{ duration: 0.5 }}
-          >
-            <div
-              className='text-[25px] text-[#af8b67]'
-              onClick={clickBurgerIcon}
+            <motion.div
+              animate={burgerIcon ? { rotate: -90, opacity: 1 } : { rotate: 0, opacity: 0.8 }}
+              transition={{ duration: 0.5 }}
             >
-              {burgerIcon ? <TfiClose  /> : <TfiAlignRight />}
-            </div>
-          </motion.div>
-        </nav>
-        { burgerIcon ? (
-        <div className=''>
-          <div className='flex flex-col px-2 py-[2vh] text-[#af8b67] bg-[#2d303f]  gap-5 max-sm:gap-4'>
-              <motion.button animate={{x:0}} className='border-2 rounded-lg border-[#af8b67] py-[1vh]' initial={{x:-1000}} transition={{delay:1}}>
-                <button className='text-3xl '>Mahzan ul-asror</button>
-              </motion.button>
-              <motion.button animate={{x:0}} className='border-2 rounded-lg border-[#af8b67] py-[1vh]' initial={{x:1000}} transition={{delay:1.2}}>
-                <button className='text-3xl '>Mahzan ul-asror</button>
-              </motion.button>
-              <motion.button animate={{x:0}} className='border-2 rounded-lg border-[#af8b67] py-[1vh]' initial={{x:-1000}} transition={{delay:1.4}}>
-                <button className='text-3xl '>Mahzan ul-asror</button>
-              </motion.button>
-              <motion.button animate={{x:0}} className='border-2 rounded-lg border-[#af8b67] py-[1vh]' initial={{x:1000}} transition={{delay:1.6}}>
-                <button className='text-3xl '>Mahzan ul-asror</button>
-              </motion.button>
-              <motion.button animate={{y:0,rotate:0}} className='border-2 rounded-lg border-[#af8b67] py-[1vh]' initial={{y:-5,rotate:20}} transition={{delay:1.8}}>
-                <button className='text-3xl '>Mahzan ul-asror</button>
-              </motion.button>
-          </div>
-              <div className='grid place-content-center py-[9.5vh] bg-white pb-[50vh]'>
-                <img src={hamza_img} alt="" className= ''/>
+              <div
+                className='text-[25px] text-[#af8b67]'
+                onClick={clickBurgerIcon}
+              >
+                {burgerIcon ? <TfiClose  /> : <TfiAlignRight />}
               </div>
-          </div>
+            </motion.div>
+          </nav>
+          { burgerIcon ? (
+          <div className=''>
+            <div className='flex flex-col px-2 py-[2vh] text-[#af8b67] bg-[#2d303f]  gap-5 max-sm:gap-4'>
+                <motion.button animate={{x:0}} className='border-2 rounded-lg border-[#af8b67] py-[1vh]' initial={{x:-1000}} transition={{delay:1}}>
+                  <button className='text-3xl '>Mahzan ul-asror</button>
+                </motion.button>
+                <motion.button animate={{x:0}} className='border-2 rounded-lg border-[#af8b67] py-[1vh]' initial={{x:1000}} transition={{delay:1.2}}>
+                  <button className='text-3xl '>Mahzan ul-asro</button>
+                </motion.button>
+                <motion.button animate={{x:0}} className='border-2 rounded-lg border-[#af8b67] py-[1vh]' initial={{x:-1000}} transition={{delay:1.4}}>
+                  <button className='text-3xl '>Mahzan ul-asror</button>
+                </motion.button>
+                <motion.button animate={{x:0}} className='border-2 rounded-lg border-[#af8b67] py-[1vh]' initial={{x:1000}} transition={{delay:1.6}}>
+                  <button className='text-3xl '>Mahzan ul-asror</button>
+                </motion.button>
+                <motion.button animate={{y:0,rotate:0}} className='border-2 rounded-lg border-[#af8b67] py-[1vh]' initial={{y:-5,rotate:20}} transition={{delay:1.8}}>
+                  <button className='text-3xl '>Mahzan ul-asror</button>
+                </motion.button>
+            </div>
+                <div className='grid place-content-center py-[9.5vh] bg-white pb-[50vh]'>
+                  <img src={hamza_img} alt="" className= ''/>
+                </div>
+            </div>
 
-        ) : (
-          <div className='hidden'>
+          ) : (
+            <div className='hidden'>
 
-          </div>
-        )}
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
