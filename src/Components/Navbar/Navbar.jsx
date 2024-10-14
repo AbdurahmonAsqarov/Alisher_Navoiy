9b513dca39de4f4e6d3cb1938e6194b4d0a96431
abdurahmon <abdurahmongit1@gmail.com>
2024-10-14 22:54:44 +0500

    part 1.5

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,8 +18,8 @@ const Navbar = () => {
 
   return (
     <>
-      <div>
-        <div className='container fixed'>
+      <div className='fixed container'>
+        <div className='container '>
           <nav className='flex justify-between px-[5vw] items-center bg-[#2d303f] border-b-2 border-[#af8b67]'>
             <img src={book_img} alt="Book Story" className='w-[80px]' />
 
